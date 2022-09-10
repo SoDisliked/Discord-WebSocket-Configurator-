674a9c92be2dd5147a19893674fa2a46a2f58fe6
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-09-10 12:03:09 +0300

    Add files via upload

---
+++ Main configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DislikedWebSocketConfiguration
+{
+    public enum FrameType: byte
+    {
+        Continuation,
+        Text,
+        Binary,
+        Close = 8,
+        Ping = 9,
+        Pong = 10,
+    }
+}
---
+++ WebSocket Connection.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DislikedSocketClientConfiguration
+{
+    public class WebSocketConnection : IWebSocketConnection
+    {
+        public WebSocketConnection(ISocket socket, Action<IWebSocketConnection> initialize, Funct<byte[], WebSocketHttpRequest> parseRequest, Func<WebSocketHttpRequest, IHandler> handlerFactory, Func<IEnumerable<string>, string>)
+        {
+            socket = socket;
+            OnOpen = () => { };
+            OnClose = () => { };
+            OnMessage = x => { };
+            OnBinary = x => { };
+            OnPing = x => { };
+            OnPong = x => { };
+            OnError = x => { };
+            _initialize = initialize;
+            _handlerFactory = handlerFactory;
+            _parseRequest = parseRequest;
+            _negotiationSubProtocol = negotiateSubProtocol;
+        }
+
+        public ISocket Socket { get; set; }
+
+        private readonly Action<IWebSocketConnection> _initalize;
+        private readonly Func<WebSocketHttpRequest>, IHandler> _handlerFactory;
+        private readonly Func<IEnumerable<string>, string> _negotiateSubProtocol;
+        readonly Func<byte[], WebSocketHttpRequest> _parseRequest;
+
+        public IHandler Handler { get; set; }
+
+        private bool _closing;
+        private bool _closed;
+        private const int ReadSize = 1024 * 4;
+
+        public Action OnOpen { get; set; }
+        
+        public Action OnClose { get; set; }
+
+        public Action<string> OnMessage { get; set; }
+
+        public Action<byte[]> OnBinary { get; set; }
+
+        public Action<byte[]> OnPing { get; set; }
+
+        public Action<byte[]> OnPong { get; set; }
+
+        public Action<byte[]> OnError { get; set; }
+
+        public IWebSocketConnectionInfo ConnectionInfo { get; private set; }
+
+        public bool IsAvailable
+        {
+            get { return !_closing && !_closed && Socket.Connected; }
+        }
+
+        public Task Send(string message)
+        {
+            return Send(message, Handler.FrameText);
+        }
+
+        public Task Send(byte[] message)
+        {
+            return Send(message, Handler.FrameBinary);
+        }
+
+        public Task SendPing(byte[] message)
+        {
+            return Send(message, Handler.FramePing);
+        }
+
+        public Task SendPong(byte[] message)
+        {
+            return Send(message, Handler.FramePong);
+        }
+
+        private Task Send<T>(T message, Funct<T, byte[]> createFrame)
+        {
+            if (Handler == null)
+            {
+                throw new InvalidOperationException("Cannot send before operation completed");
+            }
+
+            if (!IsAvailable)
+            {
+                const string errorMessage = "Data sent while closing or afther close.";
+                FleckLog.Warn(errorMessage);
+
+                var taskForException = new TaskCompletionSource<object>();
+                taskForException.SetException(new ConnectionNotAvailableException(errorMessage));
+                return taskForException.Task;
+            }
+
+            var bytes = createFrame(message);
+            return SendBytes(bytes);
+        }
+
+        public void StartReceiving()
+        {
+            var data = new List<Byte>(ReadSize);
+            var buffer = new byte[ReadSize];
+            Read(data, buffer);
+        }
+
+        public void Close()
+        {
+            Close(WebSocketStatusCode.NormalClosure);
+        }
+
+        public void Close(int code)
+        {
+            if (!IsAvailable)
+                return;
+
+            _closing = true;
+
+            if (Handler == null)
+            {
+                CloseSocket();
+                return;
+            }
+
+            var bytes = Handler.FrameClose(code);
+            if (bytes.Length == 0)
+                CloseSocket();
+            else
+                SendBytes(bytes, CloseSocket);
+        }
+
+        public void CreateHandler(IEnumerable<byte> data)
+        {
+            var request = _parseRequest(data.ToArray());
+            if (request == null)
+            {
+                return;
+            }
+            Handler = _handlerFactory(request);
+            if (Handler == null)
+                return;
+            var subProtocol = _negotiateSubProtocol(request.SubProtocols);
+            ConnectionInfo = WebSocketConnectionInfo.Create(request, Socket.RemoteIpAdress, Socket.RemotePort, subProtocol);
+
+            _initalize(this);
+
+            var handshake = Handler.CreateHandShake(subProtocol);
+            SendBytes(handshake, OnOpen);
+        }
+
+        private void Read(List<byte> data, byte[] buffer)
+        {
+            if (!IsAvailable)
+                return;
+
+            Socket.Receive(buffer, r =>
+            {
+                if (r <= 0)
+                {
+                    FleckLog.Debug("0 bytes read, operation closed");
+                    CloseSocket();
+                    return;
+                }
+                FleckLog.Debug(r + "Reading bytes operation ready");
+                var readBytes = buffer.Take(r);
+                if (Handler == null)
+                {
+                    Handler.Receive(readBytes);
+                }
+                else
+                {
+                    data.AddRange(readBytes);
+                    CreateHandler(data);
+                }
+
+                Read(data, buffer);
+            },
+            HandleReadError);
+        }
+
+        private void HandleReadError(Exception e)
+        {
+            if (e is AggregateException)
+            {
+                var agg = e as AggregateException;
+                HandleReadError(agg.InnerException);
+                return;
+            }
+
+            if (e is ObjectDisposedException)
+            {
+                FleckLog.Debug("Swallowing ObjectDisposedException", e);
+                return;
+            }
+
+            OnError(e);
+
+            if (e is WebSocketException)
+            {
+                FleckLog.Debug("Error while reading bytes", e);
+                Close(((WebSocketException)e).StatusCode);
+            } else if (e is IOException)
+            {
+                FleckLog.Debug("Error while reading bytes", e);
+                Close(WebSocketStatusCodes.AbnormalClosure);
+            } else
+            {
+                FleckLog.Error("Ongoing application process error", e);
+                Close(WebSocketStatusCodes.InternalServerError);
+            }
+        }
+
+        private Task SendBytes(byte[] bytes, Action callback == null)
+        {
+            return Socket.Send(bytes, () =>
+            {
+                FleckLog.Debug("Sent" + bytes.Length + "bytes");
+                if (callback != null)
+                    callback();
+            },
+            e =>
+            {
+                if (e is IOException)
+                    FleckLog.Debug("Failed to send message to sever, disconnecting", e);
+                else
+                    FleckLog.Info("Failed to send message to server, disconnecting", e);
+                CloseSocket();
+            });
+    }
+
+        private void CloseSocket()
+        {
+            _closing = true;
+            OnClose();
+            _closed = true;
+            Socket.Close();
+            Socket.Dispose();
+            _closing = false;
+        }
+}
