16ed1b072cdf51ae70886e9d50d0eacfb98eb380
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-09-11 12:47:12 +0300

    Add files via upload

---
+++ Fleck Log Config.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DislikedWebSocketConfiguration
+{
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Warn,
+        Error,
+    }
+
+    public class FleckLog
+    {
+        public static LogLevel level = LogLevel.Info;
+
+        public static Action<LogLevel, string, Exception> LogAction = (level, message, ex) =>
+        {
+            if (level >= level)
+                Console.WriteLine("{0} [{1}] {2} {3}", DateTime.Now, level, message, ex);
+        };
+
+        public static void Warn(string message, Exception ex = null)
+        {
+            LogAction(LogLevel.Error, message, ex);
+        }
+
+        public static void Error(string message, Exception ex = null)
+        {
+            LogAction(LogLevel.Error, message, ex);
+        }
+
+        public static void Debug(string message, Exception ex = null)
+        {
+            LogAction(LogLevel.Debug, message, ex);
+        }
+
+        public static void Info(string message, Exception ex = null)
+        {
+            LogAction(LogLevel.Info, message, ex);
+        }
+    }
+}
---
+++ Frame Type Configurator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DislikedWebSocketConfigurator
+{
+    public enum FrameType : byte
+    {
+        Continuation,
+        Text,
+        Binary,
+        Close = 8,
+        Ping = 9,
+        Pong = 10,
+
+    }
+}
