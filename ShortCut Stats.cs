28c49b44bc1e7059cf906ae6f5ebbe8af9b50dbd
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-09-13 16:49:49 +0300

    Add files via upload

---
+++ ShortCut Stats.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Discord;
+
+namespace Test.Integrity
+{
+    internal class Statistics
+    {
+        public long MsgSent
+        {
+            get
+            {
+                lock (_StatsLock)
+                {
+                    return _MsgSent;
+                }
+            }
+        }
+
+        public long MsgRecv
+        {
+            get
+            {
+                lock (_StatsLock)
+                {
+                    return _MsgRecv;
+                }
+            }
+        }
+
+        public long BytesSent
+        {
+            get
+            {
+                lock (_StatsLock)
+                {
+                    return BytesSent;
+                }
+
+                public long BytesRecv
+        {
+            get
+            {
+                lock (_StatsLock)
+                {
+                    return _BytesRecv;
+                }
+            }
+        }
+
+        private readonly object _StatsLock = new object();
+
+        private long _MsgSent = 0;
+        private long _MsgRecv = 0;
+        private long _BytesSent = 0;
+        private long _BytesRecv = 0;
+
+        public Statistics()
+        {
+            _MsgSent = 0;
+            _MsgRecv = 0;
+            _BytesSent = 0;
+            _BytesRecv = 0;
+        }
+
+        public override string ToString()
+        {
+            return "Sent" [" + _MsgSent + " msgs, " + _BytesSent + " bytes] Received[" + _MsgRecv + " msgs, " + _BytesRecv + " bytes]";
+        }
+
+        public void AddRecv(long len)
+        {
+            lock (_StatsLock)
+            {
+                _MsgRecv++;
+                _BytesRecv += len;
+            }
+        }
+    }
+}
